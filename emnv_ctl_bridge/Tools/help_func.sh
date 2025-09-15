672760824e2496c75928371dc175bdecc617e6ba
zzqhumen <1679112995@qq.com>
2025-09-15 17:13:35 +0800

    tunnel_TJU.world

--- emnv_ctl_bridge/Tools/help_func.sh
+++
@@ -1,8 +0,0 @@
-takeoff(){
-  drone_name=$1
-  rostopic pub /emnavi_cmd/takeoff std_msgs/String "data: '$drone_name'"
-}
-land(){
-  drone_name=$1
-  rostopic pub /emnavi_cmd/land std_msgs/String "data: '$drone_name'"
-}
